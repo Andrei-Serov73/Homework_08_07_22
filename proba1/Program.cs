c45140ba1272588b9065aacc44cfd4d602ba9ea7
Andrey Serov <kolobok-84@inbox.ru>
2022-07-09 15:36:23 +0400

    программа для сравнивания 2-х целых чисел вводимых из консоли

--- proba1/Program.cs
+++ proba1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int num1;
+Console.Write("Введите число1: "); 
+num1 = Convert.ToInt32 (Console.ReadLine());
+int num2;
+Console.Write("Введите число2: ");
+num2 = Convert.ToInt32 (Console.ReadLine());
+
+if(num1>num2)
+{
+    Console.Write("Большее число: ");
+    Console.WriteLine(num1);
+}
+else
+{
+    Console.Write("Большее число: ");
+    Console.WriteLine(num2);
+}
+ 
