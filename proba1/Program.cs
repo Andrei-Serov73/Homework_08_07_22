b4a842284132c5095c19b092f9ba501c7af53cdc
Andrey Serov <kolobok-84@inbox.ru>
2022-07-09 15:59:35 +0400

    перенес блоксхему в папку

--- proba1/Program.cs
+++ proba1/Program.cs
@@ -1,4 +1,6 @@
-﻿int num1;
+﻿// задача 2: напишите программу которая на вход принимает 2 числа и выдает какое большее а какое меньшее.
+// *решение для целых чисел.
+int num1;
 Console.Write("Введите число1: "); 
 num1 = Convert.ToInt32 (Console.ReadLine());
 int num2;
@@ -9,10 +11,14 @@ if(num1>num2)
 {
     Console.Write("Большее число: ");
     Console.WriteLine(num1);
+    Console.Write("Меньшее число: ");
+    Console.WriteLine(num2);
 }
 else
 {
     Console.Write("Большее число: ");
     Console.WriteLine(num2);
+    Console.Write("Меньшее число: ");
+    Console.WriteLine(num1);
 }
  
