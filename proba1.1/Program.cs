a75ead647cb9c89c8be2a15bb8ed7f32caabaa22
Andrey Serov <kolobok-84@inbox.ru>
2022-07-09 16:31:24 +0400

    отредактировал

---
+++ proba1.1/Program.cs
@@ -0,0 +1,19 @@
+﻿// задача 2: напишите программу которая на вход принимает 2 числа и выдает какое большее а какое меньшее.
+// *решение для целых, не одинаковых чисел которые пользователь задает изначально.
+int x = 7;
+int y = 7;
+if(x>y)
+{
+    Console.Write("Большее число: ");
+    Console.WriteLine(x);
+    Console.Write("Меньшее число: ");
+    Console.WriteLine(y);
+}
+else
+{
+    Console.Write("Большее число: ");
+    Console.WriteLine(y);
+    Console.Write("Меньшее число: ");
+    Console.WriteLine(x);
+}
+ 
--- proba1/Program.cs
+++ proba1/Program.cs
@@ -1,5 +1,5 @@
 ﻿// задача 2: напишите программу которая на вход принимает 2 числа и выдает какое большее а какое меньшее.
-// *решение для целых чисел.
+// *решение для целых,чисел которые пользователь вводит в консоли.
 int num1;
 Console.Write("Введите число1: "); 
 num1 = Convert.ToInt32 (Console.ReadLine());
@@ -7,18 +7,27 @@ int num2;
 Console.Write("Введите число2: ");
 num2 = Convert.ToInt32 (Console.ReadLine());
 
-if(num1>num2)
+if(num1!=num2)
 {
+
+    if(num1>num2)
+    {
     Console.Write("Большее число: ");
     Console.WriteLine(num1);
     Console.Write("Меньшее число: ");
     Console.WriteLine(num2);
+    }
+    else
+    {
+    Console.Write("Большее число: ");
+    Console.WriteLine(num2);
+    Console.Write("Меньшее число: ");
+    Console.WriteLine(num1);
+    }
 }
 else
 {
-    Console.Write("Большее число: ");
-    Console.WriteLine(num2);
-    Console.Write("Меньшее число: ");
-    Console.WriteLine(num1);
+    Console.WriteLine("Числа равны.");
 }
+
  
