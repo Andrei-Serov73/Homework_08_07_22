90b24938a831cf756b23c194ce7aebc4c80b292b
Andrey Serov <kolobok-84@inbox.ru>
2022-07-09 19:12:49 +0400

    начало без проверки равенства

--- zadacha2/Program.cs
+++ zadacha2/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int num1;
+Console.Write("Введите число1: "); 
+num1 = Convert.ToInt32 (Console.ReadLine());
+int num2;
+Console.Write("Введите число2: ");
+num2 = Convert.ToInt32 (Console.ReadLine());
+int num3;
+Console.Write("Введите число3: ");
+num3 = Convert.ToInt32 (Console.ReadLine());
+
+
+if(num1>num2)
+{
+        if(num1>num3)
+        {
+            Console.Write("Максимальное число: ");
+            Console.WriteLine(num1);
+        }
+
+        else
+        {
+        Console.Write("Максимальное число: ");
+        Console.WriteLine(num3);
+        }
+}
+else
+{
+        if(num2>num3)
+        {
+            Console.Write("Максимальное число: ");
+            Console.WriteLine(num2);
+        }
+        else
+        {
+            Console.Write("Максимальное число: ");
+            Console.WriteLine(num3);
+        }
+}  
