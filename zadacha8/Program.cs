008fa71def52ee8cedc6bd076706a1169e950f61
Andrey Serov <kolobok-84@inbox.ru>
2022-07-13 20:47:46 +0400

    решил задачу 8

---
+++ zadacha8/Program.cs
@@ -0,0 +1,19 @@
+﻿int N;
+Console.Write("Введите число: ");
+N = Convert.ToInt32(Console.ReadLine());
+int[] arroy = new int[N];
+int size = arroy.Length +1;
+int count = 0;
+int index = 0;
+arroy[index] = 1;
+while (count < size)
+{
+    double div = arroy[index] / 2;
+    double mod = arroy[index] % 2;
+    if (mod == 0)// или можно if(mod !=1 )
+    {
+        Console.Write($"{arroy[index],3} ");
+    }
+    arroy[index] = arroy[index] + 1;
+    count = count + 1;
+}
